a682d34c74642303847ae833200fb43027f5b57d
musagenius345 <musabadru@gmail.com>
2023-04-18 14:32:47 +0300

    html update

--- postcss.config.js
+++ postcss.config.js
@@ -1,9 +1,41 @@
-const autoprefixer = require('autoprefixer');
+const postImport = require('postcss-import')
+const postUrl = require('postcss-url')
+const autoprefixer = require('autoprefixer')
+// const precss = require('precss')
+// const font = require('postcss-font-magician')
+// const cssnano = require('cssnano')
+// const postcssNested = require('postcss-nested')
+// const postcssNestedAncestors = require('postcss-nested-ancestors')
+// const postcssCurrentSelector = require('postcss-current-selector')
+const postcssPresetEnv = require('postcss-preset-env');
+
 
 module.exports = {
   plugins: [
+    // font({
+    //   hosted: ['./assets/fonts', 'assets/fonts'],
+    //   display: "swap"
+    // }),
+    postImport({
+      
+    }),
+    postUrl({
+      
+    }),
     autoprefixer({
       // Autoprefixer options here
+    }),
+    
+    
+    postcssPresetEnv({
+      stage: 1,
+        features: {
+          'nesting-rules': true,
+        },
     })
+    
   ]
 };
+
+
+  
--- src/js/index.js
+++ src/js/index.js
@@ -1,13 +1,1 @@
-console.log("Hello");
-
-const end = () => {
-  console.log("Hello");
-};
-
-end();
-
-const utilityRon = () => {
-  () => {
-    console.log("hello");
-  };
-};
+console.log('Hello')
